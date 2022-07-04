e0a6b95f23cd3728d62d298c445bb961b4cbc0b0
soonzero <sykeum0912@naver.com>
2022-07-05 00:45:04 +0900

    Query Type, Scalar Types, Root Types

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+    firstName: String
+    lastName: String
+  }
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
+  }
+`
 
 const server = new ApolloServer({typeDefs});
 
