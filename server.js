7919452dd9f05ff86fde7cce0ba52d30eaf93724
soonzero <sykeum0912@naver.com>
2022-07-05 15:35:01 +0900

    Documentation

--- server.js
+++ server.js
@@ -32,25 +32,70 @@ let users = [
 ]
 
 const typeDefs = gql`
+  """
+  User의 대략적인 정보를 의미한다.
+  """
   type User {
+    """
+    해당 User의 고유 인덱스를 의미한다
+    """
     id: ID!
+    """
+    해당 User의 username을 의미한다. 
+    """
     username: String!
+    """
+    해당 User의 이름을 의미한다.
+    """
     firstName: String!
+    """
+    해당 User의 성을 의미한다.
+    """
     lastName: String!
+    """
+    User의 이름과 성을 결합한 것을 의미한다. 
+    """
     fullName: String!
   }
+  """
+  Tweet의 리소스를 나타낸다. 
+  """
   type Tweet {
+    """
+    해당 Tweet의 고유 인덱스를 나타낸다.
+    """
     id: ID!
+    """
+    해당 Tweet의 내용을 나타낸다.
+    """
     text: String!
+    """
+    해당 Tweet을 작성한 User를 나타낸다.
+    """
     author: User
   }
   type Query {
+    """
+    모든 User의 리스트를 나타낸다.
+    """
     allUsers: [User!]!
+    """
+    모든 Tweet의 리스트를 나타낸다.
+    """
     allTweets: [Tweet!]!
+    """
+    특정 인덱스에 해당하는 Tweet을 나타낸다.
+    """
     tweet(id: ID!): Tweet
   }
   type Mutation {
+    """
+    Tweet을 작성하고, 성공 시 해당 Tweet을 반환한다.
+    """
     postTweet(text: String!, userId: ID!): Tweet
+    """
+    특정 인덱스에 해당하는 Tweet을 찾아서, 있으면 삭제하고 true를 반환하며, 없으면 false를 반환한다.
+    """
     deleteTweet(id: ID!): Boolean!
   }
 `
